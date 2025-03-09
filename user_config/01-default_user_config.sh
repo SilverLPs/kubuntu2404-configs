077a7a321ad196b10195f1e68e0d7ab559ca282f
SilverLPs <36015913+SilverLPs@users.noreply.github.com>
2025-03-09 22:27:14 +0100

    Energy profile and MIME types

--- user_config/01-default_user_config.sh
+++ user_config/01-default_user_config.sh
@@ -5,7 +5,6 @@
 plasma-apply-lookandfeel -a silverlps.breezedarkcustom.desktop --resetLayout
 
 ##GGF sollten auch die Groups in Anführungszeichen gesetzt werden, austesten ob das genauso funktioniert wie davor!
-
 kwriteconfig5 --file ksplashrc --group KSplash --key Engine "none"
 kwriteconfig5 --file ksplashrc --group KSplash --key Theme "none"
 kwriteconfig5 --file klaunchrc --group FeedbackStyle --key "BusyCursor" --type bool false
@@ -20,8 +19,13 @@ kwriteconfig5 --file kscreenlockerrc --group Daemon --key "Timeout" "90"
 kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key "Image" "/usr/share/wallpapers/Next/"
 kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key "PreviewImage" "/usr/share/wallpapers/Next/"
 kwriteconfig5 --file ksmserverrc --group General --key "loginMode" "emptySession"
+kwriteconfig5 --file kcminputrc --group Keyboard --key "NumLock" "0"
+kwriteconfig5 --file plasma-nm --group General --key "ManageVirtualConnections" --type bool true
 
 busctl call org.freedesktop.Accounts /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User SetIconFile s "./ressources/face.png"
+
+#copy das Energieprofil aus den Ressources in das configdir (überschreiben!!)
+cp ./configs/powermanagementprofilesrc "$HOME/.config/powermanagementprofilesrc"
 
 #WICHTIG: Das hier sollte eigentlich in das erste Systemskript verschoben werden, sofern es mit sudo richtig arbeitet...
 busctl call com.ubuntu.WhoopsiePreferences /com/ubuntu/WhoopsiePreferences com.ubuntu.WhoopsiePreferences SetReportCrashes b false
@@ -35,7 +39,89 @@ busctl call com.ubuntu.WhoopsiePreferences /com/ubuntu/WhoopsiePreferences com.u
 #busctl --user call org.kde.kwalletd5 /modules/kwalletd5 org.kde.KWallet reconfigure
 
 
-#Dateiformate wie xml und json nicht mit Firefox, sondern mit Kate öffnen
-#Multimediaformate an VLC schicken
+#MIME type associations
+#Sollte NACH der Installation von Software erledigt werden, um es zu vermeiden, dass neue Installationen die Defaults wieder überschreiben
+xdg-mime default org.kde.discover.desktop application/vnd.debian.binary-package
 
-#Profilbild von Kubuntu auf leeres Nutzersymbol auswählen (Hintergrund so auswählen, dass es sowohl mit dem Anmeldebildschirm als auch dem Startmenü passt
+xdg-mime default org.kde.kate.desktop application/json
+xdg-mime default org.kde.kate.desktop application/xml
+
+xdg-mime default vlc.desktop application/mxf
+xdg-mime default vlc.desktop application/sdp
+xdg-mime default vlc.desktop application/vnd.adobe.flash.movie
+xdg-mime default vlc.desktop application/vnd.ms-asf
+xdg-mime default vlc.desktop application/vnd.ms-wpl
+xdg-mime default vlc.desktop application/vnd.rn-realmedia
+xdg-mime default vlc.desktop application/x-matroska
+xdg-mime default vlc.desktop application/x-netshow-channel
+xdg-mime default vlc.desktop application/x-quicktime-media-link
+xdg-mime default vlc.desktop text/x-google-video-pointer
+xdg-mime default vlc.desktop video/3gpp
+xdg-mime default vlc.desktop video/3gpp2
+xdg-mime default vlc.desktop video/dv
+xdg-mime default vlc.desktop video/mp2t
+xdg-mime default vlc.desktop video/mp4
+xdg-mime default vlc.desktop video/mpeg
+xdg-mime default vlc.desktop video/ogg
+xdg-mime default vlc.desktop video/quicktime
+xdg-mime default vlc.desktop video/vnd.avi
+xdg-mime default vlc.desktop video/vnd.mpegurl
+xdg-mime default vlc.desktop video/vnd.rn-realvideo
+xdg-mime default vlc.desktop video/webm
+xdg-mime default vlc.desktop video/x-anim
+xdg-mime default vlc.desktop video/x-flic
+xdg-mime default vlc.desktop video/x-flv
+xdg-mime default vlc.desktop video/x-javafx
+xdg-mime default vlc.desktop video/x-matroska
+xdg-mime default vlc.desktop video/x-matroska-3d
+xdg-mime default vlc.desktop video/x-ms-wmp
+xdg-mime default vlc.desktop video/x-ms-wmv
+xdg-mime default vlc.desktop video/x-nsv
+xdg-mime default vlc.desktop video/x-ogm+ogg
+xdg-mime default vlc.desktop video/x-theora+ogg
+xdg-mime default vlc.desktop x-content/video-dvd
+xdg-mime default vlc.desktop x-content/video-svcd
+xdg-mime default vlc.desktop x-content/video-vcd
+
+xdg-mime default vlc.desktop application/ogg
+xdg-mime default vlc.desktop application/x-shorten
+xdg-mime default vlc.desktop application/xspf+xml
+xdg-mime default vlc.desktop audio/aac
+xdg-mime default vlc.desktop audio/ac3
+xdg-mime default vlc.desktop audio/basic
+xdg-mime default vlc.desktop audio/flac
+xdg-mime default vlc.desktop audio/midi
+xdg-mime default vlc.desktop audio/mp2
+xdg-mime default vlc.desktop audio/mp4
+xdg-mime default vlc.desktop audio/mpeg
+xdg-mime default vlc.desktop audio/ogg
+xdg-mime default vlc.desktop audio/vnd.dts
+xdg-mime default vlc.desktop audio/vnd.dts.hd
+xdg-mime default vlc.desktop audio/vnd.rn-realaudio
+xdg-mime default vlc.desktop audio/vnd.wave
+xdg-mime default vlc.desktop audio/webm
+xdg-mime default vlc.desktop audio/x-adpcm
+xdg-mime default vlc.desktop audio/x-aiff
+xdg-mime default vlc.desktop audio/x-ape
+xdg-mime default vlc.desktop audio/x-flac+ogg
+xdg-mime default vlc.desktop audio/x-gsm
+xdg-mime default vlc.desktop audio/x-it
+xdg-mime default vlc.desktop audio/x-m4b
+xdg-mime default vlc.desktop audio/x-m4r
+xdg-mime default vlc.desktop audio/x-matroska
+xdg-mime default vlc.desktop audio/x-mod
+xdg-mime default vlc.desktop audio/x-mpegurl
+xdg-mime default vlc.desktop audio/x-ms-asx
+xdg-mime default vlc.desktop audio/x-ms-wma
+xdg-mime default vlc.desktop audio/x-musepack
+xdg-mime default vlc.desktop audio/x-opus+ogg
+xdg-mime default vlc.desktop audio/x-pn-realaudio-plugin
+xdg-mime default vlc.desktop audio/x-s3m
+xdg-mime default vlc.desktop audio/x-scpls
+xdg-mime default vlc.desktop audio/x-speex
+xdg-mime default vlc.desktop audio/x-speex+ogg
+xdg-mime default vlc.desktop audio/x-vorbis+ogg
+xdg-mime default vlc.desktop audio/x-wavpack
+xdg-mime default vlc.desktop audio/x-xm
+xdg-mime default vlc.desktop x-content/audio-cdda
+xdg-mime default vlc.desktop x-content/audio-player

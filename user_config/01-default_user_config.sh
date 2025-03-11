28abc91cb02bd30f3dde3605378cb3fe86f6ccac
silverlps <36015913+SilverLPs@users.noreply.github.com>
2025-03-11 22:00:56 +0100

    Default user config almost finished except for custom Plasma theme

--- user_config/01-default_user_config.sh
+++ user_config/01-default_user_config.sh
@@ -12,30 +12,39 @@ echo
 #Hier muss das Theme noch ins User Dir für Plasma/Lookandfeel verschoben werden
 plasma-apply-lookandfeel -a silverlps.breezedarkcustom.desktop --resetLayout
 
-##GGF sollten auch die Groups in Anführungszeichen gesetzt werden, austesten ob das genauso funktioniert wie davor!
-kwriteconfig5 --file ksplashrc --group KSplash --key Engine "none"
-kwriteconfig5 --file ksplashrc --group KSplash --key Theme "none"
-kwriteconfig5 --file klaunchrc --group FeedbackStyle --key "BusyCursor" --type bool false
-kwriteconfig5 --file kwinrc --group Plugins --key "kwin4_effect_fadingpopupsEnabled" --type bool false
-kwriteconfig5 --file kwinrc --group Plugins --key "kwin4_effect_morphingpopupsEnabled" --type bool false
-kwriteconfig5 --file kwinrc --group Effect-windowview --key "BorderActivateAll" "9"
+# kwriteconfig5 is fully idempotent and automatically creates config files and even folders if necessary, which makes mkdir or touch commands obsolete
+kwriteconfig5 --file ksplashrc --group 'KSplash' --key 'Engine' 'none'
+kwriteconfig5 --file ksplashrc --group 'KSplash' --key 'Theme' 'none'
+kwriteconfig5 --file klaunchrc --group 'FeedbackStyle' --key 'BusyCursor' --type bool false
+kwriteconfig5 --file kwinrc --group 'Plugins' --key 'kwin4_effect_fadingpopupsEnabled' --type bool false
+kwriteconfig5 --file kwinrc --group 'Plugins' --key 'kwin4_effect_morphingpopupsEnabled' --type bool false
+kwriteconfig5 --file kwinrc --group 'Effect-windowview' --key 'BorderActivateAll' '9'
 # WARNING Part of these values in kglobalshortcutsrc are in German, which could lead to problems when the systems/users language is different, it looks like most of them start in English even on a German system, and then the System settings app will change them on the fly to english as the user goes through the options, which would mean, that I could just set it to the english value with this script and on a different language system KDE will set it to the local language automatically without any problems
-kwriteconfig5 --file kglobalshortcutsrc --group kwin --key Overview "Meta+W\tMeta+Tab,Meta+W,Übersicht umschalten"
+kwriteconfig5 --file kglobalshortcutsrc --group 'kwin' --key 'Overview' 'Meta+W\tMeta+Tab,Meta+W,Übersicht umschalten'
+#Geändert auf einfache Anführungszeichen, prüfen ob \t nach sed korrekt entfernt und alles richtig ist!
 sed -i '/Overview/s/\\\\t/\\t/g' "$HOME/.config/kglobalshortcutsrc"
-kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "next activity" "none,Meta+Tab,Zwischen Aktivitäten wechseln"
-kwriteconfig5 --file kscreenlockerrc --group Daemon --key "Timeout" "90"
-kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key "Image" "/usr/share/wallpapers/Next/"
-kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key "PreviewImage" "/usr/share/wallpapers/Next/"
-kwriteconfig5 --file ksmserverrc --group General --key "loginMode" "emptySession"
-kwriteconfig5 --file kcminputrc --group Keyboard --key "NumLock" "0"
-kwriteconfig5 --file plasma-nm --group General --key "ManageVirtualConnections" --type bool true
+kwriteconfig5 --file kglobalshortcutsrc --group 'plasmashell' --key 'next activity' 'none,Meta+Tab,Zwischen Aktivitäten wechseln'
+kwriteconfig5 --file kscreenlockerrc --group 'Daemon' --key 'Timeout' '90'
+kwriteconfig5 --file kscreenlockerrc --group 'Greeter' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'Image' '/usr/share/wallpapers/Next/'
+kwriteconfig5 --file kscreenlockerrc --group 'Greeter' --group 'Wallpaper' --group 'org.kde.image' --group 'General' --key 'PreviewImage' '/usr/share/wallpapers/Next/'
+kwriteconfig5 --file ksmserverrc --group 'General' --key 'loginMode' 'emptySession'
+kwriteconfig5 --file kcminputrc --group 'Keyboard' --key 'NumLock' '0'
+kwriteconfig5 --file plasma-nm --group 'General' --key 'ManageVirtualConnections' --type bool true
 
+# Implements a basic stock profile picture for the user account to overwrite the Kubuntu icon profile picture
 busctl call org.freedesktop.Accounts /org/freedesktop/Accounts/User$(id -u) org.freedesktop.Accounts.User SetIconFile s "./ressources/face.png"
 
-kwriteconfig5 --file dolphinrc --group General --key "RememberOpenedTabs" --type bool false
+# Configures Dolphin to always start with a fresh session in users home dir
+kwriteconfig5 --file dolphinrc --group 'General' --key 'RememberOpenedTabs' --type bool false
 
-#copy das Energieprofil aus den Ressources in das configdir (überschreiben!!)
+# Configure system monitor process page to show processes of all users
+kwriteconfig5 --file "$HOME/.local/share/plasma-systemmonitor/processes.page" --group 'Face-94051759765872' --group 'org.kde.ksysguard.processtable' --group 'General' --key 'userFilterMode' '3'
+
+# Copy the energy profile from this config to the users config dir (overwrites if necessary)
 cp ./configs/powermanagementprofilesrc "$HOME/.config/powermanagementprofilesrc"
+
+# Create an Apps folder in users home dir (for AppImages and self contained installs, like /opt but on user level)
+kwriteconfig5 --file "$HOME/Apps/.directory" --group 'Desktop Entry' --key 'Icon' 'folder-appimage'
 
 #WICHTIG: Das hier sollte eigentlich in das erste Systemskript verschoben werden, sofern es mit sudo richtig arbeitet...
 busctl call com.ubuntu.WhoopsiePreferences /com/ubuntu/WhoopsiePreferences com.ubuntu.WhoopsiePreferences SetReportCrashes b false
@@ -43,15 +52,19 @@ busctl call com.ubuntu.WhoopsiePreferences /com/ubuntu/WhoopsiePreferences com.u
 busctl call com.ubuntu.WhoopsiePreferences /com/ubuntu/WhoopsiePreferences com.ubuntu.WhoopsiePreferences SetReportMetrics b false
 #Kann geprüft werden mit cat /etc/whoopsie (dort ist nur die Metrics Option) und systemctl status whoopsie.path (muss auf disabled stehen)
 
+# Install basic system tool flatpaks
 flatpak install --noninteractive flathub com.github.tchx84.Flatseal
 flatpak install --noninteractive flathub org.localsend.localsend_app
 flatpak install --noninteractive flathub org.rncbc.qpwgraph
 
-mkdir "$HOME/Apps"
-kwriteconfig5 --file "$HOME/Apps/.directory" --group 'Desktop Entry' --key 'Icon' 'folder-appimage'
+# Configure qpwgraph to not use the system tray at all (and therefore quit the process if the window is closed)
+# kwriteconfig5 really doesn't like many of the characters used in the qpwgraph config file. So this shouldn't be used on an existing config!
+echo "The following mv command is just a safety mechanism to prevent kwriteconfig5 from editing an existing file with incompatible characters. If it errors it probably means there is no config file for qpwgraph yet, which is absolutely fine"
+mv "$HOME/.var/app/org.rncbc.qpwgraph/config/rncbc.org/qpwgraph.conf" "$HOME/.var/app/org.rncbc.qpwgraph/config/rncbc.org/qpwgraph.conf.bak"-"$(date +\%Y\%m\%d)-$(date +\%H\%M\%S)"
+kwriteconfig5 --file "$HOME/.var/app/org.rncbc.qpwgraph/config/rncbc.org/qpwgraph.conf" --group 'SystemTray' --key 'Enabled' --type bool false
 
-#MIME type associations
-#Sollte NACH der Installation von Software erledigt werden, um es zu vermeiden, dass neue Installationen die Defaults wieder überschreiben
+# MIME type associations
+# This should be run after software installations to make sure new software installs don't overwrite the MIME type associations again.
 xdg-mime default org.kde.discover.desktop application/vnd.debian.binary-package
 
 xdg-mime default org.kde.kate.desktop application/json
@@ -137,7 +150,8 @@ xdg-mime default vlc.desktop audio/x-xm
 xdg-mime default vlc.desktop x-content/audio-cdda
 xdg-mime default vlc.desktop x-content/audio-player
 
-#Das deaktiviert tatsächlich KWallet, allerdings führt das zu anderen Problemen, Anwendungen wie Vivaldi speichern dann nicht mehr richtig, ggf. ist abschalten doch nicht die beste Option... Vlt. lohnt es sich das Passwort leer zu machen, was effektiv wohl die Passwortabfragen deakiviert, auch bei Autologin. Ist natürlich unsicher, allerdings ist Leerzeichen als Userpassword dann auch das Passwort des Wallets und das ist auch nicht besser, nur nerviger bei Autologin. Vlt. gibt es eine Lösung die das Wallet Passwort bei Autologin immer mitentsperrt. Sonst lohnt es sich wohlmöglich gar nicht. Sonst sollte wohlmöglich Autologin einfach nicht verwendet werden, die Einstellungen warnen eh den Nutzer davor, dass es zu Problemen mit KWallet führt, oder eine Art Autounlock wie hier https://www.reddit.com/r/kde/comments/ybp191/how_to_auto_unlock_kwallet/?tl=de oder ich könnte austesten, immer eine Nachfrage auf den Zugriff zu machen, und den vlt. pro Programm immer zu erlauben, falls das geht siehe hier grünes Feld: https://wiki.archlinux.org/title/KDE_Wallet
+# KWallet shouldn't be disabled because that can lead to severe problems with various applications (i.e. Vivaldi). If autologin is activated, the password of KWallet can be changed to just an empty string, this will disable the password prompts and KWallet will still work (but without any encryption, meaning the passwords are clear and unprotected on disk!).
+# If disabling KWallet is still desired, the following commands can be used to achieve that goal on the users own risk!
 #kwriteconfig5 --file kwalletrc --group Wallet --key "Enabled" --type bool false
 #kwriteconfig5 --file kwalletrc --group org.freedesktop.secrets --key "apiEnabled" --type bool false
 #busctl --user call org.kde.kwalletd5 /modules/kwalletd5 org.kde.KWallet reconfigure
